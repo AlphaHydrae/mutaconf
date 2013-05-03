2a933e55340d79bb75ccd3d758911d8dd042d705
AlphaHydrae <hydrae.alpha@gmail.com>
2013-05-03 20:12:50 +0200

    Removed extract.

--- lib/mutaconf.rb
+++ lib/mutaconf.rb
@@ -45,17 +45,6 @@ module Mutaconf
       memo
     end
   end
-
-  def self.extract source, key, options = {}
-    key = key.to_sym if !options.key?(:symbolize) or options[:symbolize]
-    if source.kind_of? Hash
-      source[key]
-    elsif source.kind_of?(String) or source.kind_of?(Symbol)
-      source
-    elsif source
-      source.send key
-    end
-  end
 end
 
 Dir[File.join File.dirname(__FILE__), File.basename(__FILE__, '.*'), '*.rb'].each{ |lib| require lib }
